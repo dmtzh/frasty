baeac31b7cc555e301f1402bcd94120e1231e24b
Dmitry <zhukmdmn@gmail.com>
2025-12-04 09:50:10 -0500

    Unit tests for StepHandlerAdapterFactory class

--- tests/shared/pipeline/test_stephandleradapterfactory.py
+++ tests/shared/pipeline/test_stephandleradapterfactory.py
@@ -32,7 +32,7 @@ class TestStepActionHandler[TCfg, D]:
     __test__ = False  # Instruct pytest to ignore this class for test collection
     def __call__(self, func: StepHandlerContinuation[TCfg, D]):
         self._func = func
-    def pass_result(self, result: Result[StepData[TCfg, D], Any]):
+    def pass_result(self, result: Result[StepData[TCfg, Any], Any]):
         return self._func(result)
 
 
@@ -57,6 +57,54 @@ async def test_uses_handler_creator_to_create_step_handler_adapter():
     await step_handler.pass_result(Result.Ok(expected_step_data))
 
     assert state["actual_step_input_data"] == expected_step_data
+
+
+
+async def test_when_data_validator_returns_error_then_func_not_invoked():
+    non_dict_data = ["data", "foo", "bar"]
+    step_data_with_invalid_data = StepData[None, list](RunIdValue.new_id(), StepIdValue.new_id(), TestStepDefinition(), non_dict_data, Metadata())
+    state = {}
+    async def func(v: StepData[None, dict]):
+        state["actual_step_input_data"] = v
+        return CompletedWith.Data(v.data)
+    step_handler = TestStepActionHandler[None, dict]()
+    def handler_creator(step_definition_type: StepDefinitionType[None]) -> StepHandler[None, dict]:
+        return step_handler
+    async def complete_step_func(data: CompleteStepData) -> Result:
+        return Result.Ok(None)
+
+    factory = StepHandlerAdapterFactory(handler_creator, complete_step_func)
+    adapter = factory(TestStepDefinition, TestStepDefinition.validate_input)
+    adapter(func)
+
+    await step_handler.pass_result(Result.Ok(step_data_with_invalid_data))
+
+    assert "actual_step_input_data" not in state
+
+
+
+async def test_when_data_validator_returns_error_then_complete_step_func_not_invoked():
+    expected_run_id = RunIdValue.new_id()
+    expected_step_id = StepIdValue.new_id()
+    non_dict_data = ["data", "foo", "bar"]
+    step_data_with_invalid_data = StepData(expected_run_id, expected_step_id, TestStepDefinition(), non_dict_data, Metadata())
+    async def func(v: StepData[None, dict]):
+        return CompletedWith.Data(v.data)
+    step_handler = TestStepActionHandler[None, dict]()
+    def handler_creator(step_definition_type: StepDefinitionType[None]) -> StepHandler[None, dict]:
+        return step_handler
+    state = {}
+    async def complete_step_func(data: CompleteStepData) -> Result:
+        state["actual_run_id"] = data.run_id
+        return Result.Ok(None)
+
+    factory = StepHandlerAdapterFactory(handler_creator, complete_step_func)
+    adapter = factory(TestStepDefinition, TestStepDefinition.validate_input)
+    adapter(func)
+
+    await step_handler.pass_result(Result.Ok(step_data_with_invalid_data))
+
+    assert "actual_run_id" not in state
 
 
 
