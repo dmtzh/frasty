a21e96febf61bf82c28856b3fed6a75c6774eb63
Dmitry <zhukmdmn@gmail.com>
2025-12-23 11:18:42 -0500

    Refactor execute_definition_workflow

--- definition/runner/executedefinitionhandler.py
+++ definition/runner/executedefinitionhandler.py
@@ -9,7 +9,7 @@ from shared.customtypes import DefinitionIdValue, Error, RunIdValue, StepIdValue
 from shared.definition import Definition
 from shared.infrastructure.storage.repository import StorageError
 from shared.runningdefinition import RunningDefinitionState
-from shared.utils.asyncresult import async_ex_to_error_result, async_result, coroutine_result
+from shared.utils.asyncresult import async_ex_to_error_result
 
 ToStorageActionConverter = Callable[[Callable[[RunningDefinitionState | None], tuple[RunningDefinitionState.Events.Event | None, RunningDefinitionState]]], Callable[[RunIdValue, DefinitionIdValue], Coroutine[Any, Any, RunningDefinitionState.Events.Event | None]]]
 
@@ -70,14 +70,19 @@ async def handle(
         convert_to_storage_action: ToStorageActionConverter,
         run_first_step_handler: Callable[[RunningDefinitionState.Events.StepRunning], Coroutine[Any, Any, Result]],
         cmd: ExecuteDefinitionCommand):
-    @coroutine_result[StorageError | RunFirstStepError]()
     async def execute_definition_workflow():
         run_first_step = functools.partial(_run_first_step, cmd.definition)
-        apply_run_first_step = async_result(async_ex_to_error_result(StorageError.from_exception)(convert_to_storage_action(run_first_step)))
-        opt_evt = await apply_run_first_step(cmd.run_id, cmd.definition_id)
-        if type(opt_evt) is RunningDefinitionState.Events.StepRunning:
-            await async_result(run_first_step_handler)(opt_evt).map_error(lambda err: RunFirstStepError(opt_evt.step_id, err))
-        return opt_evt
+        apply_run_first_step = async_ex_to_error_result(StorageError.from_exception)(convert_to_storage_action(run_first_step))
+        opt_evt_res = await apply_run_first_step(cmd.run_id, cmd.definition_id)
+        opt_evt = opt_evt_res.default_value(None)
+        match opt_evt:
+            case RunningDefinitionState.Events.StepRunning():
+                run_first_step_res = await run_first_step_handler(opt_evt)
+                return run_first_step_res\
+                    .map(lambda _: opt_evt)\
+                    .map_error(lambda err: RunFirstStepError(opt_evt.step_id, err))
+            case _:
+                return opt_evt_res
     async def clean_up_failed_execute(error: StorageError | RunFirstStepError):
         match error:
             case RunFirstStepError(step_id=step_id, error=error):
