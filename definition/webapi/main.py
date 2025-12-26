51a1ea1f282a4dd9d9b3bd8f36376a231bc27bef
Dmitry <zhukmdmn@gmail.com>
2025-12-26 09:00:37 -0500

    Remove legacy get manual run state by id web api

--- definition/webapi/main.py
+++ definition/webapi/main.py
@@ -2,9 +2,7 @@ from expression import Result
 from fastapi import HTTPException
 from fastapi.responses import FileResponse
 
-from manualrunstate import ManualRunStateAdapter
-from manualrunstore import manual_run_storage
-from shared.customtypes import DefinitionIdValue, RunIdValue
+from shared.customtypes import DefinitionIdValue
 from shared.definitionsstore import definitions_storage
 from shared.dtodefinition import DefinitionAdapter as LegacyDefinitionAdapter
 from shared.utils.asyncresult import async_catch_ex
@@ -33,20 +31,5 @@ async def legacy_get_definition(id: str):
         case Result(ResultTag.OK, ok=(definition, _)):
             definition_dto = LegacyDefinitionAdapter.to_list(definition)
             return definition_dto
-        case _:
-            raise HTTPException(status_code=503, detail="Oops... Service temporary unavailable, please try again later.")
-
-@app.get("/definition/legacy/manual-run/{id}")
-async def legacy_get_status(id: str):
-    opt_run_id = RunIdValue.from_value_with_checksum(id)
-    if opt_run_id is None:
-        raise HTTPException(status_code=404)
-    opt_state_res = await async_catch_ex(manual_run_storage.get)(opt_run_id)
-    match opt_state_res:
-        case Result(ResultTag.OK, ok=None):
-            raise HTTPException(status_code=404)
-        case Result(ResultTag.OK, ok=state):
-            state_dto = ManualRunStateAdapter.to_dict(state)
-            return state_dto
         case _:
             raise HTTPException(status_code=503, detail="Oops... Service temporary unavailable, please try again later.")
