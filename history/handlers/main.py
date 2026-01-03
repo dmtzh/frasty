227aad2f9815c15ac0f7dbff9ec47b3f023fbfd6
Dmitry <zhukmdmn@gmail.com>
2026-01-03 12:33:06 -0500

    Rename addtaskresulttohistoryhandler to addlegacytaskresulttohistoryhandler

--- history/handlers/main.py
+++ history/handlers/main.py
@@ -2,12 +2,12 @@
 
 from shared.taskpendingresultsqueue import CompletedTaskData
 
-import addtaskresulttohistoryhandler
+import addlegacytaskresulttohistoryhandler
 from config import app, task_completed_subscriber
 
 @task_completed_subscriber
 async def add_task_result_to_history(data: CompletedTaskData):
-    add_to_history_res = await addtaskresulttohistoryhandler.handle(data)
+    add_to_history_res = await addlegacytaskresulttohistoryhandler.handle(data)
     return add_to_history_res
 
 # if __name__ == "__main__":
