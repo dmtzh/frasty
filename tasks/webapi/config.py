73428ada23631446c66e5d9635e7b8ab31e52c36
Dmitry <zhukmdmn@gmail.com>
2026-01-05 14:41:51 -0500

    Remove legacy run task web api

--- tasks/webapi/config.py
+++ tasks/webapi/config.py
@@ -4,18 +4,15 @@ from dataclasses import dataclass
 import os
 from typing import Any
 
-from expression import effect, Result
+from expression import effect
 from fastapi import FastAPI
 
 from infrastructure.rabbitmq import config
 from shared.action import Action, ActionName, ActionType
 from shared.completedresult import CompletedResult
-from shared.customtypes import DefinitionIdValue, Metadata, RunIdValue, TaskIdValue
+from shared.customtypes import DefinitionIdValue, TaskIdValue
 from shared.pipeline.actionhandler import ActionData, ActionHandlerFactory, DataDto, run_action_adapter
-from shared.pipeline.handlers import DefinitionCompletedSubscriberAdapter, map_handler, only_from
-from shared.pipeline.logging import with_input_output_logging_subscriber
-from shared.pipeline.types import CompletedDefinitionData, RunTaskData
-from shared.utils.parse import parse_from_dict, parse_value
+from shared.utils.parse import parse_from_dict
 
 run_action = config.run_action
 
@@ -48,31 +45,6 @@ def execute_task_handler(func: Callable[[ActionData[None, ExecuteTaskInput]], Co
 STORAGE_ROOT_FOLDER = os.environ['STORAGE_ROOT_FOLDER']
 ADD_DEFINITION_URL = os.environ['ADD_DEFINITION_URL']
 CHANGE_SCHEDULE_URL = os.environ['CHANGE_SCHEDULE_URL']
-
-def run_webapi_task(task_id: TaskIdValue, run_id: RunIdValue):
-    metadata = Metadata()
-    metadata.set_from("run task webapi")
-    data = RunTaskData(task_id, run_id, metadata)
-    return config.run_task(data)
-
-@dataclass(frozen=True)
-class CompletedTaskData:
-    task_id: TaskIdValue
-    run_id: RunIdValue
-    result: CompletedResult
-
-def webapi_task_completed_subscriber(func: Callable[[CompletedTaskData], Coroutine[Any, Any, Result | None]]):
-    def to_completed_task_data(data: CompletedDefinitionData):
-        task_id_res = parse_value(data.metadata, "task_id", Metadata.get_task_id)
-        return task_id_res.map(lambda task_id: CompletedTaskData(task_id, data.run_id, data.result))
-    subscriber = config.definition_completed_subscriber(None, config.RequeueChance.LOW)
-    run_task_webapi_subscriber = only_from(subscriber, "run task webapi")
-    with_logging_subscriber = with_input_output_logging_subscriber(run_task_webapi_subscriber, "webapi_task_completed")
-    webapi_task_completed_s = map_handler(
-        with_logging_subscriber,
-        lambda data_res: data_res.bind(to_completed_task_data)
-    )
-    return DefinitionCompletedSubscriberAdapter(webapi_task_completed_s)(func)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
--- tasks/webapi/main.py
+++ tasks/webapi/main.py
@@ -9,29 +9,22 @@ from fastapi import HTTPException
 from fastapi.exceptions import RequestValidationError
 
 from shared.action import ActionName, ActionType
-from shared.completedresult import CompletedResult, CompletedWith
+from shared.completedresult import CompletedWith
 from shared.customtypes import DefinitionIdValue, Error, Metadata, RunIdValue, StepIdValue, TaskIdValue
 from shared.definition import ActionDefinition, Definition
 from shared.executedefinitionaction import EXECUTE_DEFINITION_ACTION, ExecuteDefinitionInput, run_execute_definition_action
-from shared.infrastructure.storage.repository import NotFoundError, NotFoundException, StorageError
+from shared.infrastructure.storage.repository import NotFoundError, StorageError
 from shared.pipeline.actionhandler import ActionData
 from shared.task import Task
 from shared.tasksstore import tasks_storage
 from shared.utils.asyncresult import async_catch_ex, async_ex_to_error_result
-from shared.utils.result import ResultTag, lift_param
+from shared.utils.result import lift_param
 
 import addtaskapihandler
 import cleartaskscheduleapihandler
 import getrunstateapihandler
-import runtaskapihandler
 import settaskscheduleapihandler
-from webapitaskrunstate import WebApiTaskRunState
-from webapitaskrunstore import web_api_task_run_storage
-from config import ADD_DEFINITION_URL, run_action, CompletedTaskData, ExecuteTaskInput, app, execute_task_handler, run_execute_task_action, run_webapi_task, webapi_task_completed_subscriber
-
-@app.post("/tasks/legacy/{id}/run", status_code=202)
-async def run_legacy(id: str):
-    return await runtaskapihandler.handle(lambda cmd: run_webapi_task(cmd.task_id, cmd.run_id), id)
+from config import ADD_DEFINITION_URL, run_action, ExecuteTaskInput, app, execute_task_handler, run_execute_task_action
 
 @app.get("/tasks/legacy/{id}/run/{run_id}")
 async def get_legacy_run_state(id: str, run_id: str):
@@ -44,24 +37,6 @@ async def set_legacy_schedule(id: str, request: settaskscheduleapihandler.SetSch
 @app.delete("/tasks/legacy/{id}/schedule/{schedule_id}", status_code=202)
 async def clear_legacy_schedule(id: str, schedule_id: str):
     return await cleartaskscheduleapihandler.handle(id, schedule_id)
-
-@webapi_task_completed_subscriber
-async def complete_run_state_with_result(data: CompletedTaskData):
-    @async_ex_to_error_result(StorageError.from_exception)
-    @async_ex_to_error_result(NotFoundError.from_exception, NotFoundException)
-    @web_api_task_run_storage.with_storage
-    def apply_complete_run_state(state: WebApiTaskRunState | None, result: CompletedResult):
-        if state is None:
-            raise NotFoundException()
-        new_state = state.complete(result)
-        return (new_state, new_state)
-    
-    complete_run_state_res = await apply_complete_run_state(data.task_id, data.run_id, data.result)
-    match complete_run_state_res:
-        case Result(tag=ResultTag.ERROR, error=NotFoundError()):
-            return None
-        case _:
-            return complete_run_state_res
 
 # ------------------------------------------------------------------------------------------------------------
 
