939ee62e436fcebca902c9a93ff567a428919bfe
Dmitry <zhukmdmn@gmail.com>
2026-01-16 15:40:55 -0500

    to_error_list and to_ok_list utility functions for Result type

--- shared/utils/result.py
+++ shared/utils/result.py
@@ -1,6 +1,7 @@
 from collections.abc import Callable, Coroutine
 from enum import StrEnum
 from functools import wraps
+from itertools import chain
 from typing import Any, Concatenate, ParamSpec, TypeVar
 
 from expression import Result
@@ -21,3 +22,11 @@ def lift_param(func: Callable[Concatenate[T, P], Coroutine[Any, Any, Result[R, T
     def wrapper(t_res: Result[T, TErr1], *args: P.args, **kwargs: P.kwargs) -> Coroutine[Any, Any, Result[R, TErr | TErr1]]:
         return t_res.map(lambda t: func(t, *args, **kwargs)).default_with(err1_to_result)
     return wrapper
+
+def to_error_list(*results: Result[Any, TErr]) -> list[TErr]:
+    errors = map(lambda result: result.map(lambda _: []).default_with(lambda err: [err]), results)
+    return list(chain.from_iterable(errors))
+
+def to_ok_list(*results: Result[T, Any]) -> list[T]:
+    oks = map(lambda result: result.map(lambda t: [t]).default_with(lambda _: []), results)
+    return list(chain.from_iterable(oks))
