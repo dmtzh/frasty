9efdcbb4f32b4c12aad371694699b002ec1aa9e5
Dmitry <dmtzh8@gmail.com>
2026-04-28 22:16:20 -0400

    Unit tests for aggregate step in definition configuration

--- tests/definition/shared/test_runningdefinitionstateadapter.py
+++ tests/definition/shared/test_runningdefinitionstateadapter.py
@@ -1,6 +1,6 @@
 from shared.action import ActionName, ActionType
 from shared.completedresult import CompletedWith
-from shared.definition import ActionDefinition, Definition
+from shared.definition import ActionDefinition, AggregateActionDefinition, Definition
 from shared.runningdefinition import RunningDefinitionState, RunningDefinitionStateAdapter
 
 
@@ -28,6 +28,39 @@ def test_to_list_and_back():
     running_definition_state.apply_command(RunningDefinitionState.Commands.CompleteRunningStep(second_running_step_id, result2))
     running_definition_state.apply_command(RunningDefinitionState.Commands.RunNextStep())
 
+    list_state = RunningDefinitionStateAdapter.to_list(running_definition_state)
+    restored_state = RunningDefinitionStateAdapter.from_list(list_state).ok
+
+    assert type(restored_state) is RunningDefinitionState
+    assert restored_state.recent_completed_step_id() == running_definition_state.recent_completed_step_id()
+    assert restored_state.running_step_id() == running_definition_state.running_step_id()
+    assert len(restored_state.get_events()) == len(running_definition_state.get_events())
+
+
+
+def test_to_list_and_back_with_aggregate_action():
+    get_links_data = {
+        "id": "123"
+    }
+    first_step_def = ActionDefinition(ActionName("getlinks"), ActionType.CUSTOM, None)
+    second_aggr_step_def = AggregateActionDefinition(ActionName("requesturl"), ActionType.CUSTOM, None)
+    two_steps = (first_step_def, second_aggr_step_def)
+    definition = Definition(get_links_data, two_steps)
+    running_definition_state = RunningDefinitionState()
+    running_definition_state.apply_command(RunningDefinitionState.Commands.SetDefinition(definition))
+    list_state = RunningDefinitionStateAdapter.to_list(running_definition_state)
+    running_definition_state = RunningDefinitionStateAdapter.from_list(list_state).ok
+    running_definition_state.apply_command(RunningDefinitionState.Commands.RunFirstStep())
+    list_state = RunningDefinitionStateAdapter.to_list(running_definition_state)
+    running_definition_state = RunningDefinitionStateAdapter.from_list(list_state).ok
+    result1 = CompletedWith.Data([{"url": "http://localhost/1","http_method": "GET"},{"url": "http://localhost/2","http_method": "GET"}])
+    first_running_step_id = running_definition_state.running_step_id()
+    assert first_running_step_id is not None
+    running_definition_state.apply_command(RunningDefinitionState.Commands.CompleteRunningStep(first_running_step_id, result1))
+    list_state = RunningDefinitionStateAdapter.to_list(running_definition_state)
+    running_definition_state = RunningDefinitionStateAdapter.from_list(list_state).ok
+    running_definition_state.apply_command(RunningDefinitionState.Commands.RunNextStep())
+    
     list_state = RunningDefinitionStateAdapter.to_list(running_definition_state)
     restored_state = RunningDefinitionStateAdapter.from_list(list_state).ok
 
